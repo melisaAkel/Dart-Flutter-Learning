38f286b0b9770bd1362ee386bb51214aac44cebf
melisa <irem.akel@ug.bilkent.edu.tr>
2022-06-07 17:14:10 +0300

    class finished

--- User2.dart
+++ User2.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+
 class User2 {
   //when we write our properities we need to use final but to initiliaze in our constructer
   //we should first say them late otherwise it will give error message
-  final String name;
-  final int money;
+  String name;
+
+  int money;
+
   final String city;
   final int? age;
   final String? favoriteColor;
@@ -16,6 +20,23 @@ class User2 {
     this.userCode = (favoriteColor ?? 'red') +
         name; //if favorite color is null it will initiliaze as red deafault
   }
+  //getters and setters
+  String get getName => this.name;
+
+  set setName(String name) => this.name = name;
+  int get getMoney => this.money;
+
+  set setMoney(int? money) {
+    /* if (money == null) {
+      this.money = 0;
+    } else {
+      this.money = money;
+    } */
+    //this will be have same meaning with the above part
+    this.money =
+        money == null ? 0 : money; //if it is null be 0 otherwise be money
+  }
+
   //this two methods are doing the same job when we dont have anything in paranthesis we can use this notation
   bool isIdEmpty() {
     return _userId.isEmpty;
@@ -23,4 +44,8 @@ class User2 {
 
   bool isIDEmpty() => _userId.isEmpty;
   //i meant this paranthesis
+  bool get isIDDEmpty =>
+      _userId.isEmpty; //since pharanthesis is also empty we can use like this
+  //when we want to use in our code we will just do object.isIDDEmpty;
+
 }
