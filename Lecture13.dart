0a9f28635e917d6894453e56a8f35e3d54af7137
melisa <irem.akel@ug.bilkent.edu.tr>
2022-06-15 15:48:46 +0300

    lecture 13

--- Lecture13.dart
+++ Lecture13.dart
@@ -8,9 +8,26 @@ class CarModel {
   String? city;
   bool isSecondHand;
   CarModel(this.category, this.name, this.money,
-      {this.city, this.isSecondHand = true});
+      {this.city,
+      this.isSecondHand = true}); //if isSecondHand has value it wll not make it
+  //true but if we didnt give any value it will automtically make it true
+
+  String toString() => "$isSecondHand";
 }
 
 enum CarModels { bmw, mercedes, fiat, tesla }
 
-void main() {}
+void main() {
+  CarModel b = CarModel(CarModels.mercedes, "mercedes a", 100000);
+  print(b); //with my toString
+  CarModel a =
+      CarModel(CarModels.mercedes, "mercedes a", 100000, isSecondHand: false);
+  print(a);
+
+  final myCars = [
+    //we can keep our car list also like this
+    CarModel(CarModels.mercedes, "mercedes a", 100000, isSecondHand: false),
+    CarModel(CarModels.bmw, "bmw a", 100),
+    CarModel(CarModels.mercedes, "mercedes b", 300000, isSecondHand: false),
+  ];
+}
